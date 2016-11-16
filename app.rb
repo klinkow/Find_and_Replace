ddb042c76571b56018a1e2e4d354f0a56f15e544
bill klinkow and caitlin ashtari <caitlin@>
2016-11-16 14:26:11 -0800

    rename spec files in order to remove the s and make capybara functional.

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/find_and_replace')
-also_reload('*/**/*.rb')
+also_reload('lib/**/*.rb')
 
 get ('/') do
   erb(:index)
--- spec/find_and_replace_integration_spec.rb
+++ spec/find_and_replace_integration_spec.rb
@@ -4,12 +4,12 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe 'the find_and_replace path', :type => :feature do
-  it('process a user inputted string, replace a stated word with a new inputted word ') do
+  it('process a user inputted string and replace a stated word with a new inputted word') do
     visit('/')
     fill_in('sentence', :with => 'I work parttime with robot parts')
     fill_in('look_for', :with => 'part')
-    fill_in('replace', :with => 'fart')
+    fill_in('replace', :with => 'start')
     click_button('Submit!')
-    expect(page).to have_content('I work farttime with robot farts')
+    expect(page).to have_content('I work starttime with robot starts')
   end
 end
